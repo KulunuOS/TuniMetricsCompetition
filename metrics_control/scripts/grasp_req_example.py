8d9d5161cf8963c6b769b107d33bbc79d233513f
Alexandre Angleraud <alexandre.angleraud@gmail.com>
2022-05-31 01:26:14 +0300

    separate some vision and control scripts

--- metrics_control/scripts/grasp_req_example.py
+++ metrics_control/scripts/grasp_req_example.py
@@ -1,43 +1,57 @@
 #!/usr/bin/env python3
-import tf
+
+# import tf
 import time
 import rospy
+from cobot_msgs.msg import *
 from cobot_msgs.srv import *
-from sensor_msgs.msg import JointState, CameraInfo
-from cobot_vision.camera_parameters import CameraParameters
-from cobot_controllers.demo_grasp import SingleDemoGraspAction
+#from sensor_msgs.msg import JointState, CameraInfo
+#from cobot_vision.camera_parameters import CameraParameters
+#from cobot_controllers.demo_grasp import SingleDemoGraspAction
 
 if __name__ == '__main__':
 	rospy.init_node('metrics_assembly')
 	
-	listener_tf = tf.TransformListener()
+	#listener_tf = tf.TransformListener()
 
-	camera_params = CameraParameters()
-	rospy.Subscriber("/camera/color/camera_info", CameraInfo, camera_params.camera_info_callback)
+	#camera_params = CameraParameters()
+	#rospy.Subscriber("/camera/color/camera_info", CameraInfo, camera_params.camera_info_callback)
 
-	grasp_client = SingleDemoGraspAction(listener_tf, camera_params)
+	#grasp_client = SingleDemoGraspAction(listener_tf, camera_params)
 
 	detection_client = rospy.ServiceProxy('/detect_grasp_pose', GraspPoseDetection)
-	req = GraspPoseDetectionRequest()
-	objects = detection_client(req)
+	objects = detection_client()
 
-	#grasp_client.open_hand()
+	time.sleep(2)
 
-	time.sleep(5)
+	cartesian_action_service = rospy.ServiceProxy('/take_2D_cartesian_action', Take2DCartesianAction)
+	move_gripper_service = rospy.ServiceProxy("move_gripper", MoveGripper)
+	grasping_service = rospy.ServiceProxy("grasp", Grasp)
+
+    #grasp_client.open_hand()
+	move_gripper_service(20.0, 0.08) 
+
+	time.sleep(2)
 	# Grasp a gear
 	#for obj in objects:
 	#	if (obj.obj_class == 1):
-
+	print("object_detected")
+	print(objects)
 	detection = objects.detection.detections[0]
 	print(detection)	
-	grasp_client.reach_hover(detection.x, detection.y)
+	
+	# grasp_client.reach_hover(detection.x, detection.y)
+	cartesian_action_service(pose=[detection.x, detection.y])
 	# grasp_client.reach_grasp_hover_kps(detection.kps_x, detection.kps_y)
 	# grasp_client.fix_angle(detection.angle)
 	
 	#cartesian_plan, fraction = grasp_client.plan_linear_z(-0.39)
 	#grasp_client.execute_plan(cartesian_plan)
-	cartesian_plan, fraction =grasp_client.plan_linear_z(0.20)
-	grasp_client.execute_plan(cartesian_plan)
+	
+	#cartesian_plan, fraction =grasp_client.plan_linear_z(0.20)
+	#grasp_client.execute_plan(cartesian_plan)
+	# cartesian_action_service(detection.x, detection.y, 0.20)
+
 	# grasp_client.approach_grasp()
 	#grasp_client.close_hand()
 	
--- metrics_vision/scripts/object_detection.py
+++ metrics_vision/scripts/object_detection.py
@@ -5,11 +5,11 @@ import rospy
 import numpy as np
 
 from sensor_msgs.msg import Image, CameraInfo
-
+from cobot_msgs.msg import *
 from cobot_msgs.srv import GraspPoseDetection
 from cobot_vision.detectron_model import DetectronModel
 from cobot_vision.stream_monitor import ImageStreamMonitor
-from cobot_vision.camera_parameters import CameraParameters
+
 
 
 def callback(image_data, stream_monitor):
@@ -23,11 +23,8 @@ def callback(image_data, stream_monitor):
 if __name__ == '__main__':
     rospy.init_node('metrics_vision')
 
-    camera_params = CameraParameters()
-    rospy.Subscriber("/camera/color/camera_info", CameraInfo, camera_params.camera_info_callback)
-
     object_locator = DetectronModel("/home/panda2/catkin_ws/src/tuni-panda/TuniMetricsCompetition/metrics_vision/models/metrics_model.pth")
-    stream_monitor = ImageStreamMonitor(object_locator, camera_params)
+    stream_monitor = ImageStreamMonitor(object_locator)
     
     detection_server = rospy.Service('/detect_grasp_pose', GraspPoseDetection, stream_monitor.image_analyze)
     
