892b6e38ef82d4139a0d51eeb973bd648486db93
Alexandre Angleraud <alexandre.angleraud@gmail.com>
2022-06-10 17:09:33 +0300

    integrate assembly steps

--- metrics_control/scripts/assemble_top_casing.py
+++ metrics_control/scripts/assemble_top_casing.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
 
 			input("If the robot is in the start position type enter to grasp. Otherwise - rosrun metrics_control move_to_start.py")
 
-			objects = detection_client()
+			
 
 			# Looking for 2 detections
 			# 1. Detecting the inverted top casing inv
@@ -55,11 +55,12 @@ if __name__ == '__main__':
 			bottom_detection = False 
 
 			while not (casing_detection and  bottom_detection):
+				objects = detection_client()
 				for obj in objects.detection.detections:
-					if obj.obj_class == 7 :
+					if obj.obj_class != 9:
 						print("Top casing inv found")
 						casing_detection = obj
-					elif obj.obj_class == 9:
+					if obj.obj_class == 9:
 						bottom_detection = obj 					
 				print("Cannot locate the objects. Trying again.")
 				time.sleep(1)
@@ -74,11 +75,31 @@ if __name__ == '__main__':
 			time.sleep(3)
 			rotate_ee_service(angle= casing_detection.angle)
 			time.sleep(3)
-			cartesian_action_service_1D(z_pose=0.201)
-
-			grasping_service(width=0.0043, force=20.0)
+			cartesian_action_service_1D(z_pose=0.3)
+			cartesian_action_service_1D(z_pose=0.20133,slow=True)
+			grasping_service(width=0.0019, force=20.0)
 			time.sleep(3)
-			cartesian_action_service_1D(z_pose=0.40)
+			cartesian_action_service_1D(z_pose=0.45)
+
+			# MOVE ON TOP Bottom_casing + gears assembly
+
+
+			print ('Moving on to top casing')
+			cartesian_action_service_2D(pose=[bottom_detection.x+0.004, bottom_detection.y+0.00385])
+			time.sleep(3)
+			rotate_ee_service(angle= bottom_detection.angle)
+			time.sleep(3)
+			cartesian_action_service_1D(z_pose=0.21375, slow=True)
+			move_gripper_service(20.0, 0.8)				
+	
+
+			cartesian_action_service_1D(z_pose=0.30)
+			move_gripper_service(20.0, 0.0)	
+			cartesian_action_service_1D(z_pose=0.2095, slow=True)
+			cartesian_action_service_1D(z_pose=0.45)
+			move_gripper_service(20.0, 0.8)	
+
+
 			
 
 	except rospy.ROSInterruptException:
--- metrics_control/scripts/complete_assembly.py
+++ metrics_control/scripts/complete_assembly.py
@@ -5,11 +5,22 @@ import rospy
 import math
 from cobot_msgs.msg import *
 from cobot_msgs.srv import *
+from franka_msgs.msg import FrankaState
+from franka_msgs.msg import ErrorRecoveryActionGoal
 
 
+safety_on = True
+error = False 
+
+def checking_collisions(msg, error_pub):
+	global safety_on
+	global error
+	mode = msg.robot_mode
+	if mode == 4 and not safety_on:
+		error_pub.publish(ErrorRecoveryActionGoal())
+		error = True
 
 def main():
-
 
 	detection_client = rospy.ServiceProxy('/detect_grasp_pose', GraspPoseDetection)
 	
@@ -20,20 +31,6 @@ def main():
 	move_to_joint_target = rospy.ServiceProxy('/take_action', TakeAction)
 	grasping_service = rospy.ServiceProxy("grasp", Grasp)
 
-	def grasp(x, y, angle, high=False):
-		cartesian_action_service_2D(pose=[x, y])
-		time.sleep(3)
-		rotate_ee_service(angle=angle)
-		time.sleep(3)
-		cartesian_action_service_1D(z_pose=0.202)
-
-		# grasping_service(width=0.048, force=20.0)
-		grasping_service(width=0.0245, force=20.0)
-		time.sleep(3)
-		if not high:
-			cartesian_action_service_1D(z_pose=0.40)
-		else:
-			cartesian_action_service_1D(z_pose=0.65)
 
 	# Make sure the gripper is working properly
 	try:
@@ -42,7 +39,7 @@ def main():
 	except rospy.ServiceException as exc:
 		print("Gripper not operational")
 	else:
-
+		global safety_on
  		#####################################################################
 		#						Beginning of the task 						#
 		#####################################################################
@@ -75,19 +72,27 @@ def main():
 			time.sleep(3)
 			rotate_ee_service(angle=0.0)
 			time.sleep(3)
-			cartesian_action_service_1D(z_pose=0.201)
+			cartesian_action_service_1D(z_pose=0.3)
+			cartesian_action_service_1D(z_pose=0.201, slow=True)
 			time.sleep(3)
-			cartesian_action_service_2D(pose=[obj2.x, obj2.y-0.01], slow=True)
+			# Push left
+			cartesian_action_service_2D(pose=[obj2.x, obj2.y-0.03], slow=True)
 		elif (obj1.y > obj2.y):  # first object is on the left
-			cartesian_action_service_2D(pose=[obj1.x, obj1.y+0.05], slow=False)
+			cartesian_action_service_2D(pose=[obj1.x, obj1.y+0.06], slow=False)
 			time.sleep(3)
 			rotate_ee_service(angle=0.0)
 			time.sleep(3)
-			cartesian_action_service_1D(z_pose=0.201)
+			cartesian_action_service_1D(z_pose=0.3)
+			cartesian_action_service_1D(z_pose=0.201, slow=True)
 			time.sleep(3)
-			cartesian_action_service_2D(pose=[obj2.x, obj2.y-0.01], slow=True)
+			# Push right
+			cartesian_action_service_2D(pose=[obj2.x, obj2.y+0.03], slow=True)
+
 
 		##### MOVE TO START 
+
+		cartesian_action_service_1D(z_pose=0.40)
+
 		ready_pose = [0.00, -0.25*math.pi, 0.00, -0.75 * math.pi, 0.00, 0.50 * math.pi, 0.25 * math.pi]
 		req = TakeActionRequest()
 		req.q = ready_pose
@@ -113,7 +118,18 @@ def main():
 			print("No detection")
 			time.sleep(1)
 
-		grasp(gear_detection.x, gear_detection.y, gear_detection.angle, high=True)
+		cartesian_action_service_2D(pose=[gear_detection.x, gear_detection.y])
+		time.sleep(3)
+		rotate_ee_service(angle=gear_detection.angle)
+		time.sleep(3)
+		safety_on = False
+		cartesian_action_service_1D(z_pose=0.3)
+		cartesian_action_service_1D(z_pose=0.2015, slow=True)
+		safety_on = True
+		# grasping_service(width=0.048, force=20.0)
+		grasping_service(width=0.0245, force=20.0)
+		time.sleep(3)
+		cartesian_action_service_1D(z_pose=0.55)
 
 		# Bring them above the back plate
 
@@ -126,6 +142,86 @@ def main():
 		cartesian_action_service_1D(z_pose=0.21, slow=True)
 		move_gripper_service(20.0, 0.08)
 
+		# Gently push the gears down to make sure they are correctly assembled
+		safety_on = False
+		cartesian_action_service_1D(z_pose=0.30)
+		move_gripper_service(20.0, 0.0)	
+		cartesian_action_service_1D(z_pose=0.215, slow=True)
+		cartesian_action_service_1D(z_pose=0.45)
+		move_gripper_service(20.0, 0.8)	
+		safety_on = True
+
+		#print(detection)
+		
+		#print("detections : " , detections)
+		print ('Picking the top casing')
+		
+		global error 
+		dontmoveon = True 
+		while(dontmoveon):
+			error = False 
+			##### MOVE TO START 
+			ready_pose = [0.00, -0.25*math.pi, 0.00, -0.75 * math.pi, 0.00, 0.50 * math.pi, 0.25 * math.pi]
+			req = TakeActionRequest()
+			req.q = ready_pose
+			move_to_joint_target(req)
+			time.sleep(2)
+			cartesian_action_service_2D(pose=[0.48, 0.00], slow=False)
+
+		
+			##### BRING THE TOP CASING ON TOP 
+			time.sleep(5)
+				
+			casing_detection = False 
+			bottom_detection = False 
+
+			while not (casing_detection and  bottom_detection):
+				objects = detection_client()
+				for obj in objects.detection.detections:
+					if obj.obj_class != 9:
+						print("Top casing inv found")
+						casing_detection = obj
+					if obj.obj_class == 9:
+						bottom_detection = obj 					
+				print("Cannot locate the objects. Trying again.")
+				time.sleep(1)
+
+			safety_on = False
+			cartesian_action_service_2D(pose=[casing_detection.x, casing_detection.y])
+			time.sleep(3)
+			rotate_ee_service(angle= casing_detection.angle)
+			time.sleep(3)
+			cartesian_action_service_1D(z_pose=0.3)
+			cartesian_action_service_1D(z_pose=0.20133,slow=True)
+			if not error:
+				dontmoveon = False
+			else:
+				time.sleep(2) 
+
+		grasping_service(width=0.0019, force=20.0)
+		time.sleep(3)
+		cartesian_action_service_1D(z_pose=0.45)
+
+		# MOVE ON TOP Bottom_casing + gears assembly
+
+
+		print ('Moving on to top casing')
+		cartesian_action_service_2D(pose=[bottom_detection.x+0.004, bottom_detection.y+0.00385])
+		time.sleep(3)
+		rotate_ee_service(angle= bottom_detection.angle)
+		time.sleep(3)
+		cartesian_action_service_1D(z_pose=0.21375, slow=True)
+		move_gripper_service(20.0, 0.8)				
+
+
+		cartesian_action_service_1D(z_pose=0.30)
+		move_gripper_service(20.0, 0.0)	
+		for i in range(3):
+			cartesian_action_service_1D(z_pose=0.2095, slow=True)
+		cartesian_action_service_1D(z_pose=0.45)
+		time.sleep(1)
+		move_gripper_service(20.0, 0.8)	
+			
 		##### MOVE TO START 
 		ready_pose = [0.00, -0.25*math.pi, 0.00, -0.75 * math.pi, 0.00, 0.50 * math.pi, 0.25 * math.pi]
 		req = TakeActionRequest()
@@ -134,10 +230,11 @@ def main():
 		time.sleep(2)
 		cartesian_action_service_2D(pose=[0.48, 0.00], slow=False)
 
-	
 
 if __name__ == '__main__':
 	rospy.init_node('metrics_assembly')
+	error_pub = rospy.Publisher('/franka_control/error_recovery/goal', ErrorRecoveryActionGoal, queue_size=10)
+	rospy.Subscriber('/franka_state_controller/franka_states', FrankaState, checking_collisions, (error_pub))
 	time.sleep(3)
 	main()
 	rospy.spin()
